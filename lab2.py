96327ac0dd7d1389fa404f621a7470cda08e1291
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-08-18 16:23:12 +0300

    task4

--- lab2.py
+++ lab2.py
@@ -26,23 +26,34 @@ from random import randint
 #     else: 
 #         print("try to choose another answer")
 #Task 2
-rand_a = randint(1,10)
-rand_b = randint(1,10)
-rand_c = randint(1,10)
-print(rand_a , rand_b, rand_c)
-if rand_a + rand_b > rand_c and rand_a + rand_c > rand_b and rand_b + rand_c > rand_a:
-    print("triangle exist")
-    if rand_a == rand_b or rand_b == rand_c or rand_a == rand_c:
-        print("triangle is isosceles")
-    elif rand_a == rand_b == rand_c:
-        print("triangle is equilateral")
-    elif rand_a != rand_b != rand_c:
-        print("triangle is versatile")
-else:
-    print("triangle is not exist")
-
-
-        
-        
+# rand_a = randint(1,10)
+# rand_b = randint(1,10)
+# rand_c = randint(1,10)
+# print(rand_a , rand_b, rand_c)
+# if rand_a + rand_b > rand_c and rand_a + rand_c > rand_b and rand_b + rand_c > rand_a:
+#     print("triangle exist")
+#     if rand_a == rand_b or rand_b == rand_c or rand_a == rand_c:
+#         print("triangle is isosceles")
+#     elif rand_a == rand_b == rand_c:
+#         print("triangle is equilateral")
+#     elif rand_a != rand_b != rand_c:
+#         print("triangle is versatile")
+# else:
+#     print("triangle is not exist")
+#Task 4
+rand_numb = randint(50,100)
+answer_numb = 0
+answer_numb = rand_numb
+while answer_numb != 1:
+    print('-> ',answer_numb, end=' ')
+    print(answer_numb)
+    if answer_numb == 1:
+        print("hypothesis is working!")
     
-
+    if answer_numb % 2 == 0:
+        answer_numb = answer_numb // 2
+        print(answer_numb)
+    else:
+        answer_numb = (answer_numb * 3 + 1) // 2
+        print(answer_numb, end=' ')
+print()
