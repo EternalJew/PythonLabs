b4aad63811150b7894802ba5a273107739550ec7
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-08-30 16:28:43 +0300

    add

---
+++ Part 2/task1.py
@@ -0,0 +1,29 @@
+print("TASK 1: multiplication table")
+# start the 21s task
+# x = int(input("enter x: "))
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for i in list:
+#     z = x * i
+#     print(x, '*', i, '=', z)
+    
+print("TASK 2: Напишіть функцію, яка підносить число до степеня, не використовуючи при цьому вбудовані математичні функції.")
+# a = int(input("number: "))
+# b = int(input("степінь: "))
+# n = 1
+# nsave = a
+# while n != b:
+#     a*= nsave
+#     n += 1
+
+# print(a)
+
+print("TASK 3: Напишіть функцію, яка рахує факторіал числа, використовуючи цикли")
+# n = int(input("enter: "))
+# k = 1
+# for i in range(1,n+1):
+#     k*=i
+# print(k)
+
+print("TASK 4: динамічний масив підраховує кількість від'ємних")
+
