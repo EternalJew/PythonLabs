278f248fbd555e0b93851dc3437534bfb5056917
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-12-16 13:15:56 +0200

    complete authorization

--- Flask_Website_Auth/website/auth.py
+++ Flask_Website_Auth/website/auth.py
@@ -16,6 +16,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('logged in successfully!', category = 'success')
+                login_user(user, remember = True)
                 return redirect(url_for('views.home'))
             else:
                 flash('incorrect password, try again!', category = 'error')
@@ -23,7 +24,7 @@ def login():
             flash('incorrect email, try again!', category = 'error')
         
         
-    return render_template("login.html", boolean = True)
+    return render_template("login.html", user = current_user)
 
 @auth.route('/logout')
 @login_required
@@ -55,8 +56,9 @@ def sign_up():
             new_user = User(email = email, first_name = first_name, password = generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember = True)
             flash('account created!', category = 'success')
             return redirect(url_for('views.home'))
             
     
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user = current_user)
