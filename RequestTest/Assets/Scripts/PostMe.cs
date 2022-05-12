c8dd6b165379df19d378221e3c1f1a5cd3d4d181
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2022-05-12 16:02:21 +0300

    add psot

--- RequestTest/Assets/Scripts/PostMe.cs
+++ RequestTest/Assets/Scripts/PostMe.cs
@@ -8,32 +8,109 @@ using UnityEngine.UI;
 
 public class PostMe : MonoBehaviour
 {
-    public InputField questID;
-    public InputField questName;
-    public InputField questDesc;
+    //QUEST
+    public InputField quest_name;
+    public InputField quest_description;
     public InputField questReqLvl;
+    public InputField list_of_tasks;
+    public InputField quest_reward_coins;
+    public InputField quest_reward_XP;
+    
+    //TASK
+    public InputField task_name;
+    public InputField task_description;
+    public InputField task_reward_coins;
+    public InputField task_reward_XP;
+    public InputField npc_name;
+    public InputField npc_phrase;
+    public InputField player_variant_answer;
+    public InputField npc_answer;
+
+    //ASSET
+    public InputField asset_bundle_name;
+    public InputField bundle_name;
+    public InputField asset_name;
+
+
+
 
     [Serializable]
     public class Quest
     {
-        public int questID;
-        public string questName;
-        public string questDesc;
-        public int questReqLvl;
+        //QUEST
+        public string quest_name;
+        public string quest_description;
+        public int quest_required_lvl;
+        public string list_of_tasks;
+        public int quest_reward_coins;
+        public int quest_reward_XP;
+
+        //TASK
+        public string task_name;
+        public string task_description;
+        public int task_reward_coins;
+        public int task_reward_XP;
+        public string npc_name;
+        public string npc_phrase;
+        public string player_variant_answer;
+        public string npc_answer;
+        
+        //ASSET
+        public string asset_bundle_name;
+        public string bundle_name;
+        public string asset_name;
+
+
     }
 
     public void PostData()
     {
-        int questIDFromInput = Int32.Parse(questID.text);
-        string questNameFromInput = questName.text;
-        string questDescFromInput = questDesc.text;
-        int questReqLvlFromInput = Int32.Parse(questReqLvl.text);
+        //QUEST
+        string quest_name_FromInput = quest_name.text;
+        string quest_description_FromInput = quest_description.text;
+        int quest_required_lvl_FromInput = Int32.Parse(quest_required_lvl.text);
+        string list_of_tasks_FromInput = list_of_tasks.text;
+        int quest_reward_coins_FromInput = Int32.Parse(quest_reward_coins.text);
+        int quest_reward_XP_FromInput = Int32.Parse(quest_reward_XP.text);
+        //TASK
+        string task_name_FromInput = task_name.text;
+        string task_description_FromInput = task_description.text;
+        int task_reward_coins_FromInput = Int32.Parse(task_reward_coins.text);
+        int task_reward_XP_FromInput = Int32.Parse(task_reward_XP.text);
+        string npc_name_FromInput = npc_name.text;
+        string npc_phrase_FromInput = npc_phrase.text;
+        string player_variant_answer_FromInput = player_variant_answer.text;
+        string npc_answer_FromInput = npc_answer.text;
 
+        //ASSET
+        string asset_bundle_name_FromInput = asset_bundle_name.text;
+        string bundle_name_FromInput = bundle_name.text;
+        string asset_name_FromInput = asset_name.text;
+
+
+        //QUEST
         Quest quest = new Quest();
-        quest.questID = questIDFromInput;
-        quest.questName = questNameFromInput;
-        quest.questDesc = questDescFromInput;
-        quest.questReqLvl = questReqLvlFromInput;
+        quest.quest_name = quest_name_FromInput;
+        quest.quest_description = quest_description_FromInput;
+        quest.quest_required_lvl = quest_required_lvl_FromInput;
+        quest.list_of_tasks = list_of_tasks_FromInput;
+        quest.quest_reward_coins = quest_reward_coins_FromInput;
+        quest.quest_reward_XP = quest_reward_XP_FromInput;
+        //TASK
+        quest.task_name = task_name_FromInput;
+        quest.task_description = task_description_FromInput;
+        quest.task_reward_coins = task_reward_coins_FromInput;
+        quest.task_reward_XP = task_reward_XP_FromInput;
+        quest.npc_name = npc_name_FromInput;
+        quest.npc_phrase = npc_phrase_FromInput;
+        quest.player_variant_answer = player_variant_answer_FromInput;
+        quest.npc_answer = npc_answer_FromInput;
+
+        //ASSET
+        quest.asset_bundle_name = asset_bundle_name_FromInput;
+        quest.bundle_name = bundle_name_FromInput;
+        quest.asset_name = asset_name_FromInput;
+
 
         string json = JsonUtility.ToJson(quest);
         StartCoroutine(PostRequest("http://127.0.0.1:5000/list_add", json));
